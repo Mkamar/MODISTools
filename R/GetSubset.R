e1c01f15f3d355ce5fc47cd24d4f75125c819c22
Mkamar <qamar.dr@gmail.com>
2016-07-29 16:12:09 -0400

    Update GetSubset.R

--- R/GetSubset.R
+++ R/GetSubset.R
@@ -38,7 +38,7 @@ function(Lat, Long, Product, Band, StartDate, EndDate, KmAboveBelow, KmLeftRight
   reader <- basicTextGatherer()
   header <- basicTextGatherer()
   
-  curlPerform(url = "http://daac.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.pl",
+  curlPerform(url = "http://daacmodis.ornl.gov/cgi-bin/MODIS/GLBVIZ_1_Glb_subset/MODIS_webservice.pl",
               httpheader = header.fields,
               postfields = getsubset.xml,
               writefunction = reader$update,
